ff1fe1bce3e7f27bb2ba0e338b5f9273c76ecfa5
Gabriel Adrian <adrianmendiola329@gmail.com>
2024-04-05 08:32:04 -0600

    Se sube la interfaz de UsuarioPerfil_Vendedor, Se crean las carpetas de style, css del mismo modulo, ademas de las carpeta de bootstrap

---
+++ src/java/org/utl/dsm/Controller/ControllerCliente.java
@@ -0,0 +1,21 @@
+package org.utl.dsm.Controller;
+
+import com.mysql.cj.jdbc.CallableStatement;
+import org.glassfish.grizzly.Connection;
+import org.utl.dsm.ConnectionDB.ConnectioDB;
+import org.utl.dsm.Model.Cliente;
+import org.utl.dsm.Model.Usuario;
+
+/**
+ *
+ * @author adria
+ */
+public class ControllerCliente {
+
+    public void modificarDatosCliente(Cliente cliente) {
+        
+    }
+    
+}
+
+
